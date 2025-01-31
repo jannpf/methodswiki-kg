3ef817336a00507283af5d68f6fe091ede29811c
jannpf <pfeiferjann@gmail.com>
2025-01-31 01:18:22 +0100

    Update local volume path

--- db/neo4j_docker.sh
+++ db/neo4j_docker.sh
@@ -4,7 +4,7 @@ docker run \
     --detach \
     --restart always \
     --publish=7474:7474 --publish=7687:7687 \
-    --volume=./data:/data \
+    --volume=data:/data \
     --name=neo4j_wiki \
     --env NEO4J_AUTH=neo4j/12341234 \
     neo4j:5.26.1
